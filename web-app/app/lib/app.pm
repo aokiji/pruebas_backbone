9c36e2c2f428be5003b0d84750a8510a86fb17ce
Nicolas De los Santos <ndls05@gmail.com>
2015-10-08 21:51:05 +0200

    Version inicial usando dancer

---
+++ web-app/app/lib/app.pm
@@ -0,0 +1,36 @@
+package app;
+use Dancer2;
+
+our $VERSION = '0.1';
+
+set serializer => 'JSON';
+
+get '/' => sub {
+    template 'index';
+};
+
+get '/clientes_eolica' => sub {
+    return {
+        "list" => [
+            {
+                "nombre_cliente"                => "Chorrilandia",
+                "tipo_cliente"                  => "eolica",
+                "avisar_parques_sin_actualizar" => 1
+            },
+            {
+                "nombre_cliente"                => "AERSA",
+                "tipo_cliente"                  => "eolica",
+                "avisar_parques_sin_actualizar" => 1
+            },
+            {
+                "nombre_cliente"                => "Acciona",
+                "tipo_cliente"                  => "eolica",
+                "avisar_parques_sin_actualizar" => 1
+            }
+        ],
+        "success" => "true"
+    };
+};
+
+
+true;
